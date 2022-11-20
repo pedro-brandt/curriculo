617a0034464d62c058343509fd0e74c64ae33547
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2022-11-20 14:17:58 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function jogar() {
+    var resp = document.getElementById("resp");
+    resp.innerHTML = Math.floor(1 + 6 * Math.random());
+    
+  }
+  function jogar2() {
+    var resp2 = document.getElementById("resp2");
+    resp2.innerHTML = Math.floor(1 + 6 * Math.random());
+  }
+  function jogar3() {
+    var resp3 = document.getElementById("resp3");
+    resp.innerHTML = Math.floor(0 * Math.random());
+    resp2.innerHTML = Math.floor(0 * Math.random());
+  }
